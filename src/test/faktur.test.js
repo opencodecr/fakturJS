00a4117736d26552756d7ff0b9435f98d3e01811
Open Code <opencode506@gmail.com>
2020-08-17 01:01:40 +0000

    Update test

--- src/test/faktur.test.js
+++ src/test/faktur.test.js
@@ -1,5 +1,5 @@
 const Faktur = require('../index');
 
 test('Return base value and percentage value from total value', () => {
-    expect(Faktur.rvft(0.13, 11718.83)).toStrictEqual({ baseValue: 10370.68, percentageValue: 1348.19 });
+    expect(Faktur.rvft(13, 11718.83)).toStrictEqual({ baseValue: 10370.68, percentageValue: 1348.19 });
 })
